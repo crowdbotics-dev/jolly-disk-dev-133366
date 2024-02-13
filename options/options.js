04ebf4ee062b3709ed2837d647070bec38986866
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 18:17:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-disk-dev-133366",
+  url: "https://jolly-disk-dev-133366.botics.co",
+  api: "https://jolly-disk-dev-133366.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
